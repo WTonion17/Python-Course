982f3dbcb19ab8e523d7ac3d4e53c8f31c01530e
WTonion17 <vanthanh1752k3@gmail.com>
2024-09-22 12:14:34 +0700

    update

--- contest4/main.py
+++ contest4/main.py
@@ -1,10 +1,37 @@
 from math import *
 
-def bintodec(n):
-    if n == 0: return 0
-    else: bintodec(n // 2)
-    print(n % 2,end= '')
+# def bai14(n):
+#     cnt = 0 
+#     if n < 10: return 1
+#     return 1 + bai14(n // 10)
+
+# def bai15(n):
+#     if n < 10: return n
+#     return bai15(n // 10)
+
+# def bai16_maxdigit(n):
+#     if n < 10: return n
+#     return max(n % 10, bai16_maxdigit(n // 10))
+
+# def bai16_mindigit(n):
+#     if n < 10: return n
+#     return min(n % 10, bai16_mindigit(n // 10))
+
+def bai17_left(n):
+    if n < 10: print(n, end = ' ')
+    else: 
+        bai17_left(n // 10) 
+        print(n % 10,end = ' ')
+
+
+def bai17_right(n):
+    if n < 10: print(n, end= ' ')
+    else: 
+        print(n % 10,end = ' ')
+        bai17_left(n // 10) 
 
 if __name__ == '__main__':
     n = int(input())
-    bintodec(n)
+    bai17_left(n)
+    print()
+    bai17_right(n)
