216b70a03687bd8fa86d4c1acb107caaf64c7778
WTonion17 <vanthanh1752k3@gmail.com>
2024-09-22 20:46:40 +0700

    update

--- contest4/main.py
+++ contest4/main.py
@@ -55,17 +55,65 @@ from math import *
 #         return 1  
 #     else: return 0\
 
-def bai20(n):
-    if n == 1: return 0
-    res1, res2,res3 = 10**9,10**9,10**9
-    if n % 2 == 0: 
-        res1 = bai20(n // 2) + 1
-    if n % 3 == 0: 
-        res2 = bai20(n // 3) + 1
-    if n > 1: 
-        res3 = bai20(n - 1) + 1
-    return min(res1,res2,res3)
+# def bai20(n):
+#     if n == 1: return 0
+#     res1, res2,res3 = 10**9,10**9,10**9
+#     if n % 2 == 0: 
+#         res1 = bai20(n // 2) + 1
+#     if n % 3 == 0: 
+#         res2 = bai20(n // 3) + 1
+#     if n > 1: 
+#         res3 = bai20(n - 1) + 1
+#     return min(res1,res2,res3)
 
-if __name__ == '__main__':
+# def bai21(a, left, right):
+#     if left >= right:
+#         return True
+#     else:
+#         if a[left] != a[right]:
+#             return False
+#         else:
+#             return bai21(a, left + 1, right - 1)
+
+# def left_print(a, n):
+#     if n >= 1:
+#         left_print(a, n - 1)
+#         print(a[n - 1], end=" ")
+
+
+# def right_print(a, n):
+#     if n >= 1:
+#         print(a[n - 1], end=" ")
+#         right_print(a, n - 1)
+
+# def bai23(a, n):
+#     if n >= 1:
+#         if a[n - 1] % 2 != 0:
+#             return False
+#         return bai23(a, n - 1)
+#     else:
+#         return True
+
+# def bai24(a,n):
+#     if n >= 2:
+#         if a[n - 1] <= a[n - 2]: return False
+#         return bai24(a,n - 1)
+#     else: return True
+
+def binary_search(a,x,n):
+    if n >= 1:
+        if a[n - 1] == x: return True
+        return binary_search(a,x,n-1)
+
+if __name__ == "__main__":
     n = int(input())
-    print(bai20(n))
+    a = list(map(int, input().split()))
+    x = int(input())
+    if binary_search(a,x,n):
+        print("1")
+    else:
+        print("0")
+
+
+    
+    
